df46a61bd6c978bbe4df58e937f75d2d1c705a11
sweet-poteto <sagarpardhi.dev@outlook.com>
2023-11-10 14:11:58 +0530

    add layout

---
+++ app/(dashboard)/_components/mobile-sidebar.tsx
@@ -0,0 +1,16 @@
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
+import { Menu } from "lucide-react";
+import { Sidebar } from "./sidebar";
+
+export const MobileSidebar = () => {
+  return (
+    <Sheet>
+      <SheetTrigger className="pr-4 transition md:hidden hover:opacity-75">
+        <Menu />
+      </SheetTrigger>
+      <SheetContent side="left" className="p-0 bg-white">
+        <Sidebar />
+      </SheetContent>
+    </Sheet>
+  );
+};
