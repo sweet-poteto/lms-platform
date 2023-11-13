a15c49bf6f5c1a826746acc9704d4ab8ce1def04
sweet-poteto <sagarpardhi.dev@outlook.com>
2023-11-13 11:49:53 +0530

    add dynamic layout

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -10,7 +10,7 @@ const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
       <div className="fixed inset-y-0 z-50 flex-col hidden w-56 md:flex">
         <Sidebar />
       </div>
-      <main className="h-full md:pl-56">{children}</main>
+      <main className="h-full md:pl-56 pt-[80px]">{children}</main>
     </div>
   );
 };
