46e5266612f46016107c353d7c6ec9f555d571fd
sweet-poteto <sagarpardhi.dev@outlook.com>
2023-11-14 12:13:12 +0530

    add course title form

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
@@ -1,9 +1,66 @@
-export default function CourseIdPage({
+import { db } from "@/lib/db";
+import { auth } from "@clerk/nextjs";
+import { LayoutDashboard } from "lucide-react";
+import { redirect } from "next/navigation";
+
+import { IconBadge } from "@/components/icon-badge";
+import { TitleForm } from "./_components/title-form";
+
+export default async function CourseIdPage({
   params,
 }: {
   params: {
     courseId: string;
   };
 }) {
-  return <div>CourseIdPage: {params.courseId}</div>;
+  const { userId } = auth();
+
+  if (!userId) {
+    return redirect("/");
+  }
+
+  const course = await db.course.findUnique({
+    where: {
+      id: params.courseId,
+    },
+  });
+
+  if (!course) {
+    return redirect("/");
+  }
+
+  const requiredField = [
+    course.title,
+    course.description,
+    course.price,
+    course.categoryId,
+  ];
+
+  const totalFields = requiredField.length;
+  const completedFields = requiredField.filter(Boolean).length;
+
+  const completionText = `(${completedFields}/${totalFields})`;
+
+  return (
+    <div className="p-6">
+      <div className="flex items-center justify-between">
+        <div className="flex flex-col gap-y-2">
+          <h1 className="text-2xl font-medium">Course Setup</h1>
+          <span className="text-sm text-slate-700">
+            Complete all fields {completionText}
+          </span>
+        </div>
+      </div>
+
+      <div className="grid grid-cols-1 gap-6 mt-16 md:grid-cols-2">
+        <div>
+          <div className="flex items-center gap-x-2">
+            <IconBadge icon={LayoutDashboard} />
+            <h2 className="text-xl">Customize your course</h2>
+          </div>
+          <TitleForm initialData={course} courseId={course.id} />
+        </div>
+      </div>
+    </div>
+  );
 }
