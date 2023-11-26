0ed54b44b137313ac0f93608e6cd80979be40ac4
sweet-poteto <sagarpardhi.dev@outlook.com>
2023-11-26 12:46:29 +0530

    add course attachment form

---
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/attachment-form.tsx
@@ -0,0 +1,124 @@
+"use client";
+
+import * as z from "zod";
+import Image from "next/image";
+import axios from "axios";
+import toast from "react-hot-toast";
+import { File, ImageIcon, Loader2, Pencil, PlusCircle, X } from "lucide-react";
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+
+import { Attachment, Course } from "@prisma/client";
+import { Button } from "@/components/ui/button";
+import { FileUpload } from "@/components/file-upload";
+
+interface AttachmentFormProps {
+  initialData: Course & { attachments: Attachment[] };
+  courseId: string;
+}
+
+const formSchema = z.object({
+  url: z.string().min(1),
+});
+
+export const AttachmentForm = ({
+  initialData,
+  courseId,
+}: AttachmentFormProps) => {
+  const router = useRouter();
+  const [isEditing, setIsEditing] = useState(false);
+  const [deletingId, setDeletingId] = useState<string | null>(null);
+
+  const toggleEdit = () => setIsEditing((current) => !current);
+
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    try {
+      await axios.post(`/api/courses/${courseId}/attachments`, values);
+      toast.success("Course updated");
+      toggleEdit();
+      router.refresh();
+    } catch (error) {
+      console.log(error);
+      toast.error("Something went wrong");
+    }
+  };
+
+  const onDelete = async (id: string) => {
+    try {
+      setDeletingId(id);
+      await axios.delete(`/api/courses/${courseId}/attachments/${id}`);
+      toast.success("Attachement deleted");
+      router.refresh();
+    } catch (error) {
+      console.log(error);
+      toast.error("Something went wrong");
+    }
+  };
+
+  return (
+    <div className="p-4 mt-6 border rounded-md bg-slate-100">
+      <div className="flex items-center justify-between font-medium">
+        Course attachments
+        <Button variant="ghost" onClick={toggleEdit}>
+          {isEditing && <>Cancel</>}
+          {!isEditing && (
+            <>
+              <PlusCircle className="w-4 h-4 mr-2" />
+              Add an file
+            </>
+          )}
+        </Button>
+      </div>
+      {!isEditing && (
+        <>
+          {initialData.attachments.length === 0 && (
+            <p className="mt-2 text-sm italic text-slate-500">
+              No attachments yet
+            </p>
+          )}
+          {initialData.attachments.length > 0 && (
+            <div className="space-y-2">
+              {initialData.attachments.map((attachment) => (
+                <div
+                  key={attachment.id}
+                  className="flex items-center w-full p-3 border rounded-md bg-sky-100 border-sky-200 text-sky-700"
+                >
+                  <File className="flex-shrink-0 w-4 h-4 mr-2" />
+                  <p className="text-xs line-clamp-1">{attachment.name}</p>
+                  {deletingId === attachment.id && (
+                    <div>
+                      <Loader2 className="w-4 h-4 animate-spin" />
+                    </div>
+                  )}
+                  {deletingId !== attachment.id && (
+                    <button
+                      className="ml-auto transition hover:opacity-75"
+                      onClick={() => onDelete(attachment.id)}
+                    >
+                      <X className="w-4 h-4" />
+                    </button>
+                  )}
+                </div>
+              ))}
+            </div>
+          )}
+        </>
+      )}
+      {isEditing && (
+        <div>
+          <FileUpload
+            endpoint="courseAttachments"
+            onChange={(url) => {
+              if (url) {
+                onSubmit({ url: url });
+              }
+            }}
+          />
+          <div className="mt-4 text-xs text-muted-foreground">
+            Add anything your students might need to complete the course.
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
